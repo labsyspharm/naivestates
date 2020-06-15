4f9820c718a06a657b6e1039eac77f97653e6994
Artem Sokolov <artem.sokolov@gmail.com>
2020-06-15 17:56:53 -0400

    UMAP summary plot

--- R/umap.R
+++ R/umap.R
@@ -21,6 +21,7 @@ makePal <- function( v )
 #' @param excl character vector of column names to exclude from modeling
 #' @return X, augmented with columns UMAP1, UMAP2
 #' @importFrom magrittr %>%
+#' @export
 umap <- function( X, excl )
 {
     trm <- intersect( excl, colnames(Y) )
@@ -29,3 +30,28 @@ umap <- function( X, excl )
         dplyr::mutate_all( ~(.x - min(.x))/(max(.x) - min(.x)) )
     dplyr::bind_cols(X, U)
 }
+
+#' Summary UMAP plot with points colored by cell type (dominant label)
+#' @param U data frame produced by umap()
+#' @return ggplot object encapsulating the plot
+#' @export
+plotSummary <- function( U )
+{
+    v <- c("State", "Anchor") %in% colnames(U)
+    if( all(v) && length(unique(U$Anchor)) <= 24 )
+    {
+        U <- dplyr::mutate( U, Label = stringr::str_c(State, " (", Anchor, ")") )
+        pal <- makePal( Z$Label )
+        
+        ## Plot UMAP projection, coloring by cell state calls
+        ggplot2::ggplot( U, ggplot2::aes(UMAP1, UMAP2, color=Label) ) +
+            ggplot2::geom_point() + ggplot2::theme_bw() +
+                ggplot2::scale_color_manual( values=pal,
+                                            name="Cell State Call\n(Dominant Marker)" )
+    }
+    else
+        ggplot2::ggplot( U, aes(UMAP1, UMAP2) ) +
+            ggplot2::theme_bw() + ggplot2::geom_point(color="gray")
+}
+
+
