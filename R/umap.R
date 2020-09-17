d5766bd9a701c85c4ae91005695c15f1516d56eb
Artem Sokolov <artem.sokolov@gmail.com>
2020-09-17 16:17:20 -0400

    Moved all plotting functions to the same file

--- R/umap.R
+++ R/umap.R
@@ -31,48 +31,4 @@ umap <- function( X, excl )
     dplyr::bind_cols(na.omit(X), U)
 }
 
-#' Summary UMAP plot with points colored by cell type (dominant label)
-#' @param U data frame produced by umap()
-#' @return ggplot object encapsulating the plot
-#' @export
-plotSummary <- function( U )
-{
-    v <- c("State", "Anchor") %in% colnames(U)
-    if( all(v) && length(unique(U$Anchor)) <= 24 )
-    {
-        U <- dplyr::mutate( U, Label = stringr::str_c(State, " (", Anchor, ")") )
-        pal <- makePal( U$Label )
-        
-        ## Plot UMAP projection, coloring by cell state calls
-        ggplot2::ggplot( U, ggplot2::aes(UMAP1, UMAP2, color=Label) ) +
-            ggplot2::geom_point() + ggplot2::theme_bw() +
-                ggplot2::scale_color_manual( values=pal,
-                                            name="Cell State Call\n(Dominant Marker)" )
-    }
-    else
-        ggplot2::ggplot( U, aes(UMAP1, UMAP2) ) +
-            ggplot2::theme_bw() + ggplot2::geom_point(color="gray")
-}
-
-#' Faceted UMAP plot showing probabilities of individual markers
-#' @param U data frame produced by umap()
-#' @param excl character vector of column names to exclude from plotting
-#' @return ggplot object of the faceted plot
-#' @importFrom magrittr %>%
-#' @export
-plotProbs <- function( U, excl )
-{
-    trm <- intersect( excl, colnames(U) )
-    Z <- U %>% dplyr::select( -dplyr::one_of(trm) ) %>%
-        tidyr::gather( Marker, Probability, -UMAP1, -UMAP2 )
-
-    nmrk <- length( unique(Z$Marker) )
-    
-    ggplot2::ggplot( Z, ggplot2::aes(UMAP1, UMAP2, color=Probability) ) +
-        ggplot2::geom_point(size=0.5) + ggplot2::theme_void() +
-        ggplot2::facet_wrap( ~Marker, ncol=round(sqrt(nmrk)) ) +
-        ggplot2::scale_color_gradient2(midpoint=0.5,
-                                       high=scales::muted("red"),
-                                       low=scales::muted("blue"))
-}
 
